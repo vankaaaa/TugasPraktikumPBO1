aa3f4d0051398bb4b138abaa3d6dfad2523a9387
vankaaaa <vankaap18@gmail.com>
2022-03-09 09:19:50 +0700

    Tugas 2 PrakPBO

---
+++ Tugas2/src/Tugas2/Balok.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas2;
+
+/**
+ *
+ * @author VANKA
+ */
+public class Balok extends PersegiPanjang implements MenghitungRuang{
+    private double tinggi, hasilVolume, hasilLuasPermukaan;
+    
+    public Balok(double p, double l, double t){
+        super(p , l);
+        tinggi = t;
+    }
+    
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    @Override
+    public double hitungVolume() {
+        return super.hitungLuas() * tinggi;
+    }
+
+    @Override
+    public double hitungLuasPermukaan() {
+        return 2 * (super.hitungLuas() + (super.getLebar() * tinggi) + (super.getPanjang() * tinggi));
+    }
+}
---
+++ Tugas2/src/Tugas2/MenghitungBidang.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tugas2;
+
+/**
+ *
+ * @author VANKA
+ */
+public interface MenghitungBidang {
+    double PHI = 3.14;
+    double hitungLuas();
+    double hitungKeliling();
+}
